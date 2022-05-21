0987e1ba1b7b886a8d7d6f3ca3d92d06798c2eaa
Mulder8vidas <mzutam@autonoma.edu.pe>
2022-05-20 19:15:04 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HeaderComponent} from "./Page/header/header.component";
+import {BodyComponent} from "./Page/body/body.component";
+import {FooterComponent} from "./Page/footer/footer.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
